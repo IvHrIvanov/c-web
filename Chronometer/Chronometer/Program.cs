98b600555917dc8c3fb4cd3de853d785f8973f6b
Ivan Ivanov <71284114+IvHrIvanov@users.noreply.github.com>
2022-02-11 01:10:53 +0200

    Add files via upload

---
+++ Chronometer/Chronometer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Chronometer;
+
+var chronometer = new Chronometera();
+
+string line;
+
+while ((line = Console.ReadLine()) != "exit")
+{
+    if (line == "start")
+    {
+        Task.Run(() =>
+        {
+            chronometer.Start();
+        });
+    }
+    else if (line == "lap")
+    {
+        Console.WriteLine(chronometer.Lap());
+    }
+    else if (line == "laps")
+    {
+        if (chronometer.Laps.Count == 0)
+        {
+            Console.WriteLine("Laps no laps");
+            continue;
+        }
+        Console.WriteLine("Laps: ");
+        for (int i = 0; i < chronometer.Laps.Count; i++)
+        {
+            Console.WriteLine($"{i}, {chronometer.Laps[i]}");
+
+        }
+    }
+    else if (line == "reset")
+    {
+        chronometer.Reset();
+    }
+    else if (line == "time")
+    {
+        Console.WriteLine(chronometer.GetTime);
+    }
+
+}
+chronometer.Stop();
